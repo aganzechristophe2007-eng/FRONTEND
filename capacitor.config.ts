11220f31ccd9e3d8bbe35b631895c64578c05eb3
aganzechristophe2007-eng <aganzechristophe2007@gmail.com>
2026-09-09 07:46:58 +0200

    feat: mise à jour du composant Login et nettoyage du code

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.soko.app',
+  appName: 'CBFSOKO',
+  webDir: 'dist'
+};
+
+export default config;
